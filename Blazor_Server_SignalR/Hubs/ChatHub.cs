7ea584fd735914625a6cb1ce69838fd34dda7107
Jose Ignacio Vazquez <vazquezjoseignacio22@gmail.com>
2022-11-25 13:20:11 -0300

    Agregar archivos de proyecto.

---
+++ Blazor_Server_SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Blazor_Server_SignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+        public Task EnviarMensaje(string user, string msg) 
+        {
+
+            return Clients.All.SendAsync("RecibirMensaje", user, msg);
+        }
+    }
+}
