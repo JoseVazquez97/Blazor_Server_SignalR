e4db527486f26f69719e6461f641d2ffee0c3c60
Jose Ignacio Vazquez <vazquezjoseignacio22@gmail.com>
2023-01-30 13:18:02 -0300

    Cosas nuevas

--- Blazor_Server_SignalR/wwwroot/js/indexmio.js
+++ Blazor_Server_SignalR/wwwroot/js/indexmio.js
@@ -1,1 +1,12 @@
-﻿
+﻿var booleano = false;
+
+function topFunction() {
+	var info = document.getElementById('informacion');
+	booleano = !booleano;
+
+	if (booleano) {
+		info.style.visibility = "visible";
+	} else {
+		info.style.visibility = "hidden";
+	}
+}
